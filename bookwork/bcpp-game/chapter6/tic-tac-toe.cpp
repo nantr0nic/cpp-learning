de3e3502060b51f5acf2f6c391f973960efc50dc
nantr0nic <andy15givingup@gmail.com>
2025-01-18 16:15:51 -0800

    chapter 6

---
+++ bookwork/bcpp-game/chapter6/tic-tac-toe.cpp
@@ -0,0 +1,17 @@
+/* Pseudocode Plan:
+Create an empty Tic-Tac-Toe board
+Display the game instructions
+Determine who goes first
+Display the board
+While nobody has won and it's not a tie
+    If it's the human's turn
+        Get the human's move
+        Update the baord with the human's move
+    Otherwise
+        Calculate the computer's move
+        Update the board with the computer's move
+    Display the board
+    Switch turns
+Congratulate the winner or declare a tie
+*/
+
