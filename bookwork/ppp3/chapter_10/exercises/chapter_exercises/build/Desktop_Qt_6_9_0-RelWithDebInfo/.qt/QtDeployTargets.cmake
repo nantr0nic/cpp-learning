2dc3f3ce7b4a7b9cd38bdcdeff3f4489151d6eb2
nantr0nic <andy15givingup@gmail.com>
2025-05-24 14:22:16 -0700

    Removing Qt build folders from tracking

--- bookwork/ppp3/chapter_10/exercises/chapter_exercises/build/Desktop_Qt_6_9_0-RelWithDebInfo/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Programming_Qt_FILE /home/nantr0nic/code/cpp-learning/bookwork/ppp3/chapter_10/exercises/chapter_exercises/build/Desktop_Qt_6_9_0-RelWithDebInfo/Programming_Qt)
-set(__QT_DEPLOY_TARGET_Programming_Qt_TYPE EXECUTABLE)
--- bookwork/ppp3/chapter_11/scratchpad_book_stuff/build/Desktop_Qt_6_9_0-RelWithDebInfo/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Programming_Qt_FILE /home/nantr0nic/code/cpp-learning/bookwork/ppp3/chapter_11/scratchpad_book_stuff/build/Desktop_Qt_6_9_0-RelWithDebInfo/Programming_Qt)
-set(__QT_DEPLOY_TARGET_Programming_Qt_TYPE EXECUTABLE)
