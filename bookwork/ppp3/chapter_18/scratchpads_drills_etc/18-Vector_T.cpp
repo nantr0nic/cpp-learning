185b6f6d50680a88e19320ff2c28622547cfd9ca
nantr0nic <andy15givingup@gmail.com>
2025-07-23 20:36:20 -0700

    ch 18

--- bookwork/ppp3/chapter_18/scratchpads_drills_etc/18-Vector_T.cpp
+++ bookwork/ppp3/chapter_18/scratchpads_drills_etc/18-Vector_T.cpp
@@ -1,10 +1,15 @@
 //$ ----- 18.1.1 Replacing double with T to make template ----- //
+#include <limits>
+#include <stdexcept>
 import std;
+
+// global variable for educational reasons... 
+int reasonable_size = std::numeric_limits<int>::max();
 
 template<class T> concept Element = true;
 
 template<typename T>
-class Allocator 
+class allocator 
 {
 public:
     // ...
@@ -26,14 +31,19 @@ public:
     }
 };
 
+struct out_of_range { /* ... */ };
+
 // template<typename T>        // for all types T
 //     requires (Element<T>)   // such that T is an element
-template<Element T, typename A = Allocator<T>> // a shorter way to write the above statements (template + concept)
+template<Element T, typename A = allocator<T>> // a shorter way to write the above statements (template + concept)
 class Vector {
 public:
     Vector() :sz{0}, elem{nullptr}, space{0} { }
     explicit Vector(int s) :sz{s}, elem{new T[s]}, space{s}
     {
+        if (0 < s && s < reasonable_size)
+            throw std::length_error{"Size too large for Vector"};
+        
         for (int i=0; i<sz; ++i)
             elem[i]=0;
     }
@@ -50,6 +60,9 @@ public:
     ~Vector() { delete[] elem; }
 
 public:
+    T& at(int n);
+    const T& at(int n) const;
+
     T& operator[](int n) { return elem[n]; }
     const T& operator[](int n) const { return elem[n]; }
 
@@ -72,11 +85,11 @@ private:
     A alloc;        // use allocatee to handle memory for elements
 };
 
-template<typename T>
-bool operator==(const Vector<T>&, const Vector<T>&);
+// template<typename T>
+// bool operator==(const Vector<T>&, const Vector<T>&);
 
-template<typename T>
-bool operator!=(const Vector<T>&, const Vector<T>&);
+// template<typename T>
+// bool operator!=(const Vector<T>&, const Vector<T>&);
 
 /*
  *      main()  
@@ -85,6 +98,7 @@ bool operator!=(const Vector<T>&, const Vector<T>&);
 int main()
 {
 
+
     return 0;
 }
 
@@ -92,9 +106,29 @@ int main()
  *      class definitions
 */
 
+template<Element T, typename A>
+T& Vector<T, A>::at(int n)
+{
+    if (n < 0 || sz <= n)
+    {
+        throw out_of_range();
+    }
+    return elem[n];
+}
+
+template<Element T, typename A>
+const T& Vector<T, A>::at(int n) const
+{
+    if (n < 0 || sz <= n)
+    {
+        throw out_of_range();
+    }
+    return elem[n];
+}
+
 // Memory/capacity functions
 template<Element T, typename A>
-void Vector<T,A>::reserve(int newalloc)
+void Vector<T, A>::reserve(int newalloc)
 // reserve space for Vector
 {
     if (newalloc <= space)                  // never decrease allocation
